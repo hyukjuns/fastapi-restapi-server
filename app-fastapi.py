e10e6939c4458b867dd556326b22fdbc01469b39
hyukjun <hyukjuner@gmail.com>
2024-09-29 18:49:57 +0900

    update fastapi

--- app-fastapi.py
+++ app-fastapi.py
@@ -1,22 +1,49 @@
 import os
 import logging
 import socket
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
 
 app = FastAPI()
 
-# GET
-@app.get("/api/v1/info")
-async def main(name: str = None):
+@app.get("/api/v1/hostname")
+def get_hostname(request: Request):
 
+    request_info = {
+        'request_header': request.headers,
+        'request_method': request.method,
+        'request_address': request.client,
+        'request_path_params': request.path_params,
+        'request_query_params': request.query_params,
+        'request_url': request.url
+    }
+    json_compatible_item_data = jsonable_encoder(request_info)
     # Get hostname
-    hostname = socket.gethostname()
-    print(name)
-    if name is not None:
+    if os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount') or os.getenv('KUBERNETES_SERVICE_HOST') is not None:
+        pod_name = os.environ.get("HOSTNAME")
         return {
-                    "hostname": f"{hostname}",
-                    "Your Name": f"{name}"
-
-                }
+            "hostname": f"{pod_name}",
+            "requsts": f"{json_compatible_item_data}"
+            }
     else:
-        return {"hostname": f"{hostname}"}
+        local_hostname = socket.gethostname()
+        return {
+            "hostname": f"{local_hostname}", 
+            "requsts": f"{json_compatible_item_data}"
+            }
+
+# @app.get("/api/v1/hostname")
+# async def get_hostname(name: str = None):
+
+#     # Get hostname
+#     hostname = socket.gethostname()
+#     print(name)
+#     if name is not None:
+#         return {
+#                     "hostname": f"{hostname}",
+#                     "Your Name": f"{name}"
+#                 }
+#     else:
+#         return {"hostname": f"{hostname}"}
