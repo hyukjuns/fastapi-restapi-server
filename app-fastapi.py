5a403057f482a3ede35cd894530f7ec3fae0fb7b
hyukjun <hyukjuner@gmail.com>
2024-10-02 22:58:59 +0900

    update apis

--- app-fastapi.py
+++ app-fastapi.py
@@ -1,15 +1,45 @@
 import os
 import logging
 import socket
-from fastapi import FastAPI, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, HTTPException
 
 
 app = FastAPI()
 
-@app.get("/api/v1/hostname")
-def get_hostname(request: Request):
+# Fake Key Value Database
+fake_rank_db = {
+        "son": 1, 
+        "maddison": 2, 
+        "solanke": 3
+}
+
+# Get Player's Ranking by Query Parameter
+@app.get("/api/v1/ranks")
+async def read_item(player: str = None, status_code=200):
+    if player is None:
+        return fake_rank_db
+    if player not in fake_rank_db:
+        raise HTTPException(status_code=404, detail="Player not found")
+    return f"{player} is {fake_rank_db[player]} rank" 
+ 
+# Get Host Name
+@app.get("/api/v1/hostname", status_code=200)
+def get_hostname():
+
+    if os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount') or os.getenv('KUBERNETES_SERVICE_HOST') is not None:
+        pod_name = os.environ.get("HOSTNAME")
+        return {
+                "hostname": f"{pod_name}",
+        }
+    else:
+        local_hostname = socket.gethostname()
+        return {
+                "hostname": f"{local_hostname}", 
+        }
+
+# Get Request Info
+@app.get("/", status_code=200)
+async def home(request: Request):
 
     request_info = {
         'request_header': request.headers,
@@ -19,31 +49,15 @@ def get_hostname(request: Request):
         'request_query_params': request.query_params,
         'request_url': request.url
     }
-    json_compatible_item_data = jsonable_encoder(request_info)
-    # Get hostname
-    if os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount') or os.getenv('KUBERNETES_SERVICE_HOST') is not None:
-        pod_name = os.environ.get("HOSTNAME")
-        return {
-            "hostname": f"{pod_name}",
-            "requsts": f"{json_compatible_item_data}"
-            }
-    else:
-        local_hostname = socket.gethostname()
-        return {
-            "hostname": f"{local_hostname}", 
-            "requsts": f"{json_compatible_item_data}"
-            }
 
-# @app.get("/api/v1/hostname")
-# async def get_hostname(name: str = None):
+    return {
+        "request info": f"{request_info}"
+    }
 
-#     # Get hostname
-#     hostname = socket.gethostname()
-#     print(name)
-#     if name is not None:
-#         return {
-#                     "hostname": f"{hostname}",
-#                     "Your Name": f"{name}"
-#                 }
-#     else:
-#         return {"hostname": f"{hostname}"}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000,debug=True)
+# else:
+#     gunicorn_logger = logging.getLogger('gunicorn.error')
+#     app.logger.handlers = gunicorn_logger.handlers
+#     app.logger.setLevel(gunicorn_logger.level)
