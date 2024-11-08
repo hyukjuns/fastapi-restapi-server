8058fe22b32301bc9fcc6d495d869403b84b8994
hyukjun <hyukjuner@gmail.com>
2024-11-08 16:28:21 +0900

    update main.py

--- app/main.py
+++ app/main.py
@@ -85,7 +85,7 @@ async def ready():
 # Check Startup
 @app.get("/connections")
 async def connections_db():
-    return "Database Connection OK"
+    return "Database Connection OK test"
 
 
 if __name__ == "__main__":
